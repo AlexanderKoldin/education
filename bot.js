37eee352f12d53b8504dadd11c07897afdad0e4e
Alexander Koldin <78144210+EzrealLancelot@users.noreply.github.com>
2022-07-27 07:19:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,34 +5,91 @@
 // @description  For Google
 // @author       Alexander Koldin
 // @match        https://www.google.com/*
+// @match        https://napli.ru/*
+// @match        https://kiteuniverse.ru/*
+// @match        https://motoreforma.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["10 самых популярныйх шрифтов от Google", "вывод произвольных полей wordpress", "Отключение редакций и ривизий в Wordpress"];
+let sites = {
+	"napli.ru": [
+		"10 самых популярных шрифтов от Google",
+		"вывод произвольных полей wordpress",
+		"Отключение редакций и ревизий в Wordpress",
+	],
+	"kiteuniverse.ru": ["Шоу воздушных змеев", "Kite Universe", "красота, грация, интеллект"],
+	"motoreforma.com": ["мотореформа", "прошивки для CAN-AM", "тюнинг Maverick X3"],
+};
+
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
+let keywords = sites[site];
 let keyword = keywords[getRandom(0, keywords.length)];
+
 let links = document.links;
-let btnK = document.getElementsByName('btnK')[0];
-let googleInput = document.getElementsByName('q')[0];
+let googleInput = document.getElementsByName("q")[0];
+let btnK = document.getElementsByName("btnK")[0];
 
-
-if(btnK !== undefined) {
-    googleInput.value = keyword;
-    btnK.click();
+if (btnK !== undefined) {
+	document.cookie = `site=${site}`;
+} else if (location.hostname == "www.google.com") {
+	site = getCookie("site");
 } else {
+	site = location.hostname;
+}
 
-    let links = document.links
+if (btnK !== undefined) {
+	let i = 0;
+	let timerId = setInterval(function () {
+		googleInput.value += keyword[i];
+		i++;
+		if (i == keyword.length) {
+			clearInterval(timerId);
+			btnK.click();
+		}
+	}, 700);
+} else if (location.hostname == site) {
+	setInterval(() => {
+		let index = getRandom(0, links.length);
 
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].href.includes("napli.ru")) {
-            let link = links[i];
-            console.log("Нашел строку " + links[i]);
-            link.click();
-            break;
-        }
-    }
+		if (getRandom(0, 101) >= 70) {
+			location.href = "https://www.google.com/";
+		} else if (links[index].href.indexOf(site) !== -1) {
+			links[index].click();
+		}
+	}, getRandom(2000, 5000));
+} else {
+	let nextGooglePage = true;
+	for (let i = 0; i < links.length; i++) {
+		if (links[i].href.includes(site)) {
+			let link = links[i];
+			nextGooglePage = false;
+			console.log("нашел строку " + links[i]);
+			setTimeout(() => {
+				link.click();
+			}, getRandom(2000, 5000));
+
+			break;
+		}
+	}
+	if (document.querySelector(".YyVfkd").innerText == "5") {
+		nextGooglePage = false;
+		location.href = "https://www.google.com/";
+	}
+	if (nextGooglePage) {
+		setTimeout(() => {
+			pnnext.click();
+		}, getRandom(3000, 7000));
+	}
 }
 
 function getRandom(min, max) {
-    return Math.floor(Math.random()*(max - min) + min);
+	return Math.floor(Math.random() * (max - min) + min);
 }
+function getCookie(name) {
+	let matches = document.cookie.match(
+		new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)")
+	);
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
